0585ca7ce189214c37acca2ceac34b6ecf605387
Dennis Lien <dennis.lien.o@gmail.com>
2023-01-31 12:29:58 +0100

    Add basic azure key vault integration

--- packages/ymir-plugin-azure-key-vault/src/lib/ymir-plugin-azure-key-vault.spec.ts
+++ packages/ymir-plugin-azure-key-vault/src/lib/ymir-plugin-azure-key-vault.spec.ts
@@ -1,7 +1,68 @@
-import { ymirPluginAzureKeyVault } from './ymir-plugin-azure-key-vault';
+import * as keyVault from './ymir-plugin-azure-key-vault';
 
-describe('ymirPluginAzureKeyVault', () => {
-  it('should work', () => {
-    expect(ymirPluginAzureKeyVault()).toEqual('ymir-plugin-azure-key-vault');
+/**
+ * TODO:
+ * should mock the azure key vault client
+ * for now you need the az cli to be logged in to run the tests;
+ */
+
+jest.setTimeout(200000);
+describe('Ymir plugin azure key vault', () => {
+  const vaultName = 'ymir-test-vault';
+  const ctx = { vaultName };
+  const envs = [
+    { key: 'PORT', path: 'dev-test-port', value: '3000' },
+    { key: 'DEBUG', path: 'dev-test-debug', value: 'true' },
+    { key: 'TEST_VAR', path: 'dev-test-testVar', value: 'Some test value' },
+  ];
+  const cleanUp = [...envs];
+  beforeAll(async () => {
+    await Promise.all(envs.map((env) => keyVault.add(env, ctx)));
+  });
+  afterAll(async () => {
+    await Promise.all(cleanUp.map((env) => keyVault.remove(env, ctx)));
+  });
+  it('should get a secret', async () => {
+    const env = envs[0];
+    const value = await keyVault.resolveOne(env, ctx);
+    expect(value).toBe(env.value);
+  });
+  it('should get all secrets', async () => {
+    const resolved = await keyVault.resolveAll(envs, ctx);
+    expect(resolved).toEqual(envs.map((env) => [env.key, env.value]));
+  });
+  it('should add a secret', async () => {
+    const props = {
+      key: 'ADD_PROP',
+      path: 'dev-test-add-prop',
+      value: 'addProp',
+    };
+    cleanUp.push(props);
+    const result = await keyVault.add(props, ctx);
+    expect(result).toBeUndefined();
+    const value = await keyVault.resolveOne(props, ctx);
+    expect(value).toBe(props.value);
+  });
+  it('should should delete', async () => {
+    const props = {
+      key: 'REMOVE_PROP',
+      path: 'dev-test-remove-prop',
+      value: 'removeProp',
+    };
+    const result = await keyVault.add(props, ctx);
+    expect(result).toBeUndefined();
+
+    const value = await keyVault.resolveOne(props, ctx);
+    expect(value).toBe(props.value);
+    const removeResult = await keyVault.remove(props, ctx);
+    expect(removeResult).toBeUndefined();
+
+    try {
+      const postRemove = await keyVault.resolveOne(props, ctx);
+    } catch (error) {
+      expect(error).toBeDefined();
+      return;
+    }
+    throw new Error('Should have thrown an error');
   });
 });
