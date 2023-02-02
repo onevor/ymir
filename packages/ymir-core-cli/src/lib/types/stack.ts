efcfc1d6034a8ef03f476b3086967d9d4c1a9f9b
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-02 13:18:34 +0100

    Start adding stack types

---
+++ packages/ymir-core-cli/src/lib/types/stack.ts
@@ -0,0 +1,14 @@
+export type StackSource = {
+  stack: string;
+  defaultStack: string;
+  stackConfig: string;
+  defaultStackConfig: string;
+};
+
+// TODO: need to type this;
+export type StackParsed = {
+  stack: any;
+  defaultStack: any;
+  stackConfig: any;
+  defaultStackConfig: any;
+};
