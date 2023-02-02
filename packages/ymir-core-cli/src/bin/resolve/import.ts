e34320039e2dacab3e738f8f8ea57254ff2206b7
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-02 14:04:37 +0100

    Get the resolver to use

--- packages/ymir-core-cli/src/bin/resolve/import.ts
+++ packages/ymir-core-cli/src/bin/resolve/import.ts
@@ -3,8 +3,13 @@ import * as nodePath from 'path';
 import * as commandLineArgs from 'command-line-args';
 import * as dotenv from 'dotenv';
 
+import { StackSource } from '../../lib/types/stack';
+
+import * as helper from '../../lib/config/helper';
+import * as getPlugin from '../../lib/plugin/get-plugin';
+import * as getStack from '../../lib/stack/get';
 import * as fs from '../../lib/config/helper/fs';
-import { isInProject } from '../lib/index';
+import { isInProject, validateRequiredProps } from '../lib/index';
 import * as help from '../lib/help';
 
 function getEnvPath(args: any, ctx: any) {
@@ -36,12 +41,52 @@ export async function getEnvData(args: any, ctx: any) {
   }
 }
 
+export function getResolverToUse(opt: any, stackSource: StackSource) {
+  if (opt.resolver) return [null, opt.resolver];
+  const [defaultResolverErr, defaultResolver] =
+    getPlugin.defaultResolver(stackSource);
+
+  if (defaultResolverErr) return [defaultResolverErr, null];
+
+  return [null, defaultResolver];
+}
+
 export async function importStack(args: any, ctx: any) {
   const { cwd } = ctx;
   await isInProject(true, ctx);
 
-  const def = [{ name: 'path', alias: 'p', type: String }, help.def];
+  const def = [
+    { name: 'path', alias: 'p', type: String },
+    { name: 'stack', alias: 's', type: String },
+    { name: 'resolver', alias: 'r', type: String },
+    help.def,
+  ];
   const opt = commandLineArgs(def, { argv: args });
+
+  const [isValid, valMessage] = validateRequiredProps(
+    opt,
+    ['path', 'stack'],
+    ctx
+  );
+
+  if (!isValid) {
+    console.error(valMessage);
+    return;
+  }
+
+  // TODO: add help
+
+  const ymirPath = await helper.ymirProjectFolderPath(cwd);
+  const stackSource = await getStack.stackSource(ymirPath, opt.stack);
+
+  const [resolverAliasErr, resolverAlias] = getResolverToUse(opt, stackSource);
+
+  if (resolverAliasErr) {
+    console.error('No resolver was found', resolverAliasErr);
+    return;
+  }
+
+  // TODO: validate resolver
 
   const [envDataErr, envData] = await getEnvData(opt, ctx);
   if (envDataErr) {
