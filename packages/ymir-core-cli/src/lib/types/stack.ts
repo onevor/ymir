d7973a7ce1dcfac4dc4c81b4fb081ae80187573e
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-02 13:42:57 +0100

    Add get stack helper

--- packages/ymir-core-cli/src/lib/types/stack.ts
+++ packages/ymir-core-cli/src/lib/types/stack.ts
@@ -1,8 +1,24 @@
+export type FilePath = string;
+
+export type YmirPath = FilePath;
+
+// A path to a resource in the YMirPath.
+export type YmirRelativePath = FilePath;
+
+export type StackName = string;
+
 export type StackSource = {
   stack: string;
   defaultStack: string;
   stackConfig: string;
   defaultStackConfig: string;
+};
+
+export type StackSourcePaths = {
+  stack: FilePath;
+  defaultStack: FilePath;
+  stackConfig: FilePath;
+  defaultStackConfig: FilePath;
 };
 
 // TODO: need to type this;
