e53e845bc9540f84d03ba4433e1741987eeae3b1
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-03 13:08:31 +0100

    Add import env plugin handler

---
+++ packages/ymir-core-cli/src/lib/plugin/load-plugin.ts
@@ -0,0 +1,44 @@
+/**
+ * All calls to the plugins should happen here;
+ */
+
+export async function importEnvData(
+  data: Record<string, string>,
+  resolverConfig: any,
+  resolverAlias: string,
+  stackName: string
+) {
+  const hasConfig = Object.hasOwnProperty.call(resolverConfig, resolverAlias);
+  if (!hasConfig) {
+    return [
+      {
+        code: 'NOT_FOUND_PLUGIN_CONFIG',
+        message: `Plugin config not found for alias: ${resolverAlias}`,
+        alias: resolverAlias,
+        stackName,
+      },
+      null,
+    ];
+  }
+  const hasInstall = Object.hasOwnProperty.call(
+    resolverConfig[resolverAlias],
+    'installed'
+  );
+  if (!hasInstall) {
+    return [
+      {
+        code: 'INVALID_PLUGIN_CONFIG',
+        message: `Invalid plugin config for alias: ${resolverAlias}. missing installed path`,
+        alias: resolverAlias,
+        stackName,
+      },
+      null,
+    ];
+  }
+  const config = resolverConfig[resolverAlias];
+  const resolver = await import(resolverConfig[resolverAlias].installed);
+  const payload = {
+    data,
+  };
+  return resolver.importEnv(payload, config);
+}
