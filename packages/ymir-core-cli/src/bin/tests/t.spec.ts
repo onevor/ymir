353deae55d5f90c1ca3e067ee02bdde717679ee2
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-20 11:37:53 +0100

    Add todos

--- packages/ymir-core-cli/src/bin/tests/t.spec.ts
+++ packages/ymir-core-cli/src/bin/tests/t.spec.ts
@@ -1,14 +1,23 @@
-import * as nodePath from 'path';
-
 import * as test from './helper';
 
 jest.setTimeout(200000);
 
+/**
+ * Should be able to make tests like:
+ * - Create a new ymir project
+ * - create a new ymir project with no plugins
+ * - create ymir project with global plugins
+ * - create ymir project with local plugins
+ * - create ymir project with global and local plugins
+ *
+ * - test all commands
+ *
+ */
+
 describe('ymir init', () => {
   it('should create a new ymir project', async () => {
+    const testPath = await test.setupTest(test.testDirPath);
     // Need to create test dir in project root, or NPM will complain
-    const path = nodePath.resolve(__dirname, '../../../../../', 'tmp');
-    const testPath = await test.setupTest(path);
     // await test.setupScripts.npmInit(testPath);
     // await test.setupScripts.npmInstall(
     //   testPath,
