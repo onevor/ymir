23cd4d3d964615cd216e6cd8e0aa9c23ac5dc070
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-20 13:56:20 +0100

    Export new plugin methods

--- packages/ymir-core-cli/src/lib/plugin/index.ts
+++ packages/ymir-core-cli/src/lib/plugin/index.ts
@@ -1,9 +1,15 @@
 import { _export } from './export';
+import * as dynamicImport from './dynamic-import';
+import * as edit from './edit';
 import * as get from './get-plugin';
+import * as val from './validate';
 
 const plugin = {
+  dynamicImport,
+  edit,
   export: _export,
   get,
+  val,
 };
 
 export default plugin;
