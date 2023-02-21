48da6dc7e3ba395be4d1e0e6f48a87dc49711c3e
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-21 13:37:15 +0100

    Add basic logger

--- packages/ymir-core-cli/src/lib/util/logger/index.ts
+++ packages/ymir-core-cli/src/lib/util/logger/index.ts
@@ -6,3 +6,23 @@ const chalk: any = Chalk;
 export function logError(error: YmirError) {
   return console.error(`${chalk.red.bold(error.code)}: ${error.message}`);
 }
+
+const logMsgFormat = (msg) => msg;
+
+type ConsoleLike = {
+  log: (...args: any[]) => void;
+  info: (...args: any[]) => void;
+  warn: (...args: any[]) => void;
+  error: (...args: any[]) => void;
+};
+
+export const logger: ConsoleLike = {
+  log: (message: string, ...args: any[]) =>
+    console.log(logMsgFormat(message), ...args),
+  info: (message: string, ...args: any[]) =>
+    console.info(logMsgFormat(message), ...args),
+  warn: (message: string, ...args: any[]) =>
+    console.warn(logMsgFormat(message), ...args),
+  error: (message: string, ...args: any[]) =>
+    console.error(logMsgFormat(message), ...args),
+};
