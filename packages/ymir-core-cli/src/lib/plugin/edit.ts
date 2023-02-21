46a1bb67f1dc4985f5cea8f1bacae064b9d33169
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-21 10:43:45 +0100

    Remove prop from secret store

--- packages/ymir-core-cli/src/lib/plugin/edit.ts
+++ packages/ymir-core-cli/src/lib/plugin/edit.ts
@@ -1,5 +1,6 @@
 export async function add(resolver: any, prop: any, ctx: any) {
   // TODO: validate prop;
+  // TODO: validate that plugin has implemented method;
   try {
     const result = await resolver.add(prop, ctx);
     return [null, result];
@@ -8,6 +9,27 @@ export async function add(resolver: any, prop: any, ctx: any) {
       {
         code: 'RESOLVER_ADD_ERROR',
         message: 'Error adding property',
+        originalError: error,
+        context: {
+          ctx,
+        },
+      },
+      null,
+    ];
+  }
+}
+
+export async function remove(resolver: any, prop: any, ctx: any) {
+  // TODO: validate prop;
+  // TODO: validate that plugin has implemented method;
+  try {
+    const result = await resolver.remove(prop, ctx);
+    return [null, result];
+  } catch (error) {
+    return [
+      {
+        code: 'RESOLVER_REMOVE_ERROR',
+        message: 'Error removing property',
         originalError: error,
         context: {
           ctx,
