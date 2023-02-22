ec5fe0c15735f89a5e2ab4104812b71cee4c8ae0
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-22 10:31:38 +0100

    Export schema

--- packages/ymir-core-cli/src/lib/stack/index.ts
+++ packages/ymir-core-cli/src/lib/stack/index.ts
@@ -1,9 +1,11 @@
 import * as get from './get';
 import * as update from './update';
+import * as schema from './schema';
 
 const stack = {
   get,
   update,
+  schema,
 };
 
 export default stack;
