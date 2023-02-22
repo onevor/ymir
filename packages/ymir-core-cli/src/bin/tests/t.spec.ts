6545890d369ecb4cc3f35c7fc0706be966202856
Dennis Lien <dennis.lien.o@gmail.com>
2023-02-22 15:19:15 +0100

    WIP tests

--- packages/ymir-core-cli/src/bin/tests/t.spec.ts
+++ packages/ymir-core-cli/src/bin/tests/t.spec.ts
@@ -13,19 +13,36 @@ jest.setTimeout(200000);
  * - test all commands
  *
  */
-
-describe('ymir init', () => {
-  it('should create a new ymir project', async () => {
-    const testPath = await test.setupTest(test.testDirPath);
-    // Need to create test dir in project root, or NPM will complain
-    // await test.setupScripts.npmInit(testPath);
-    // await test.setupScripts.npmInstall(
-    //   testPath,
-    //   '@onevor/ymir-plugin-ssm',
-    //   true
-    // );
-    await test.setupScripts.ymirInit(testPath);
-    await test.setupScripts.ymirRun(testPath, '-v');
-    // await test.teardownTest(testPath);
+describe('ymir', () => {
+  describe('ymir init', () => {
+    it('should create a new ymir project with out plugins', async () => {
+      const testPath = await test.setupTest(test.testDirPath);
+      // Need to create test dir in project root, or NPM will complain
+      // await test.setupScripts.npmInit(testPath);
+      // await test.setupScripts.npmInstall(
+      //   testPath,
+      //   '@onevor/ymir-plugin-ssm',
+      //   true
+      // );
+      await test.setupScripts.ymirInit(testPath);
+      await test.setupScripts.ymirRun(testPath, '-v');
+      // await test.teardownTest(testPath);
+    });
+    it('should create a new ymir project with plugins', async () => {
+      // create new npm project
+      // install plugins
+    });
   });
+
+  describe('Edit config', () => {});
+
+  describe('Install new plugin', () => {});
+
+  describe('Import env', () => {});
+
+  describe('Add Update Remove from stack', () => {});
+
+  describe('Checkout', () => {});
+
+  describe('Export', () => {});
 });
